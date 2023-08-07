383fd21a6a6896ede970403a27c0eeddd9a7694f
morphage32 <mwigand32@yahoo.com>
2023-08-06 20:42:33 -0700

    All custom enumerable methods have been added to module. All tests pass for each method.

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,90 @@
 module Enumerable
   # Your code goes here
+  def my_all?()
+    for element in self
+      if yield(element) == false
+        return false
+      end
+    end
+
+    return true
+  end
+
+  def my_any?()
+    for element in self
+      if yield(element) == true
+        return true
+      end
+    end
+
+    return false
+  end
+
+  def my_count()
+    if block_given?
+      counter = 0
+      for element in self
+        if yield(element) == true
+          counter += 1
+        end
+      end
+      return counter
+    else
+      return self.length
+    end
+  end
+
+  def my_each_with_index()
+    if block_given?
+      i = 0
+      for element in self
+        yield(element, i)
+        i += 1
+      end
+    end
+
+    return self
+  end
+
+  def my_inject(number)
+    for element in self
+      number = yield(number, element)
+    end
+
+    return number
+  end
+
+  def my_map()
+    mapped_array = []
+
+    for element in self
+      mapped_array.push(yield(element))
+    end
+
+    return mapped_array
+  end
+
+  def my_none?()
+    for element in self
+      if yield(element) == true
+        return false
+      end
+    end
+
+    return true
+  end
+
+  def my_select()
+    selected = []
+
+    for element in self
+      if yield(element) == true
+        selected.push(element)
+      end
+    end
+
+    return selected
+  end
 end
 
 # You will first have to define my_each
@@ -8,4 +93,13 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each()
+    if block_given?
+      for element in self
+        yield(element)
+      end
+    end
+
+    return self
+  end
 end
